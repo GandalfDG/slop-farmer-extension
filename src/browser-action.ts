8d4408651620dd86a9cc724f1881f43d331bbda1
Jack Case <jackacase@gmail.com>
2025-11-08 20:37:52 +0000

    signup form submission from popup
    server needs work to support properly

--- src/browser-action.ts
+++ src/browser-action.ts
@@ -2,10 +2,16 @@ import { API_URL, send_message_to_background } from "./common.js"
 
 const login_form = document.getElementById("login-form") as HTMLFormElement
 const login_status = document.getElementById("login-status")
+
+const signup_form = document.getElementById("signup-form") as HTMLFormElement
+
 if (localStorage.getItem("accessToken")) {
     login_status.setAttribute("style", "visibility: visible;")
 }
+
 login_form.addEventListener("submit", (event) => { event.preventDefault(); submit_login_form() })
+
+signup_form.addEventListener("submit", (event) => { event.preventDefault(); submit_signup_form() })
 
 async function submit_login_form() {
 
@@ -30,5 +36,21 @@ async function submit_login_form() {
     }
     else {
         //bad login, update the form
+    }
+}
+
+async function submit_signup_form() {
+    const signup_url = new URL("/signup", API_URL)
+
+    const request = new Request(signup_url, {
+        method: "POST",
+        body: new FormData(signup_form)
+    })
+
+    const response = await fetch(request)
+
+    console.log(response)
+    if (response.ok) {
+        
     }
 }
