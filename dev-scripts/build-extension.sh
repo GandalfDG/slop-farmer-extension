f71467fdf8a6cab10ca2f9c1a55626abdfc7fee6
Jack Case <jackacase@gmail.com>
2025-12-10 17:20:08 +0000

    extension builds as I want it to.

--- dev-scripts/build-extension.sh
+++ dev-scripts/build-extension.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
-rm -r scripts/ package.zip
+rm -rf scripts/ package.zip
 
-tsc
+if tsc; then
 
-mkdir -p scripts/altcha
-cp -r node_modules/altcha/dist_external scripts/altcha/
+    mkdir -p scripts/altcha
+    cp -r node_modules/altcha/dist_external scripts/altcha/
+    cp node_modules/altcha/* scripts/altcha/
 
-mkdir -p scripts/indexeddb-promise
-cp node_modules/@coderundebug/indexeddb-promise/* scripts/indexeddb-promise
+    mkdir -p scripts/indexeddb-promise
+    cp node_modules/@coderundebug/indexeddb-promise/* scripts/indexeddb-promise
 
-zip -r package.zip icons/ pages/ scripts/ styles/ manifest.json
+    zip -r package.zip icons/ pages/ scripts/ styles/ manifest.json
+
+else
+
+    echo "tsc failed, package not built"
+
+fi 
